ee210e1d08734b5336c9e0d2ee9ad0cd97977395
corn <dalcon10280@gmail.com>
2025-03-22 19:39:57 +0900

    init

---
+++ src/main/kotlin/kr/paytogether/PaytogetherApplication.kt
@@ -0,0 +1,11 @@
+package kr.paytogether
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class PaytogetherApplication
+
+fun main(args: Array<String>) {
+    runApplication<PaytogetherApplication>(*args)
+}
