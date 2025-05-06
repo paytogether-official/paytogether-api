f89184eed07a37d66fb84d12c2a72596b0647672
corn <dalcon10280@gmail.com>
2025-05-07 00:30:21 +0900

    feat: add update journey

--- src/main/kotlin/kr/paytogether/journey/JourneyService.kt
+++ src/main/kotlin/kr/paytogether/journey/JourneyService.kt
@@ -64,20 +64,21 @@ class JourneyService(
         if (journey.closedAt != null) {
             throw BadRequestException(ErrorCode.VALIDATION_ERROR, "Journey already closed for journeyId: $journeyId")
         }
+
         val memberNameSet = journeyMemberRepository.findByJourneyId(journey.journeyId)
             .map { it.name }
             .toSet()
 
-        // 이름 중복 확인
-        if (update.members.any { memberNameSet.contains(it.name) }) {
-            throw BadRequestException(ErrorCode.VALIDATION_ERROR, "Duplicate member name found")
-        }
+        update.members
+            .filterNot { memberNameSet.contains(it.name) }
+            .map { it.toEntity(journeyId) }
+            .let { journeyMemberRepository.saveAll(it) }
+            .collect()
+
         return JourneyResponse.of(
-            journey,
-            update.members.map { it.toEntity(journeyId) }
-                .let { journeyMemberRepository.saveAll(it) }
-                .map { member -> JourneyMemberResponse.from(member) }
-                .toList()
+            journeyRepository.save(journey.copy(startDate = update.startDate, endDate = update.endDate)),
+            journeyMemberRepository.findByJourneyId(journey.journeyId)
+                .map { JourneyMemberResponse.from(it) }
         )
     }
 
